2cc79e2e83ccc78af8918286ac85b0a639bcc529
EPanfil <panfilelena82@gmail.com>
2023-07-02 00:11:21 +0300

    Implement the Driver Manager and configure the Selenium dependencies

--- src/main/java/org/opencart/TestRunner.java
+++ src/main/java/org/opencart/TestRunner.java
@@ -1,8 +1,29 @@
 package org.opencart;
 
-public class TestRunner {
-    public static void main(String[] args) {
+import org.opencart.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 
-        System.out.println("Hello world!");
+public class TestRunner {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverManager.getInstance().getDriver();
+        driver.get("https://docs.google.com/spreadsheets/d/1r4zBeEAyjE4iGTijoxUawxr0mIfBvZuBh3yIsQJG0Y0/edit#gid=0");
+
+        String currentWindowName = driver.getWindowHandle();
+
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        driver.get("https://github.com/TestAutomationPanfilElena/JavaUiAutomationFramework/branches");
+        Thread.sleep(500);
+        driver.close();
+        driver.switchTo().window(currentWindowName);
+        driver.get("https://protv.md/");
+
+        driver.quit();
+        System.out.println("The execution was finished");
+
+
     }
+
+
 }
