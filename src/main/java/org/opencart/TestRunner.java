a6884506aa43768ad9c2cfd35f1cffb847941ebc
EPanfil <panfilelena82@gmail.com>
2023-07-02 12:35:33 +0300

    Implemented the FakeDataManager and create the first scenario.

--- src/main/java/org/opencart/TestRunner.java
+++ src/main/java/org/opencart/TestRunner.java
@@ -1,29 +1,81 @@
 package org.opencart;
 
+import org.opencart.managers.DataFakerManager;
 import org.opencart.managers.DriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
+import org.opencart.managers.ScrollManager;
+import org.openqa.selenium.*;
 
 public class TestRunner {
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = DriverManager.getInstance().getDriver();
-        driver.get("https://docs.google.com/spreadsheets/d/1r4zBeEAyjE4iGTijoxUawxr0mIfBvZuBh3yIsQJG0Y0/edit#gid=0");
-
-        String currentWindowName = driver.getWindowHandle();
-
-        driver.switchTo().newWindow(WindowType.TAB);
-
-        driver.get("https://github.com/TestAutomationPanfilElena/JavaUiAutomationFramework/branches");
-        Thread.sleep(500);
-        driver.close();
-        driver.switchTo().window(currentWindowName);
-        driver.get("https://protv.md/");
-
-        driver.quit();
-        System.out.println("The execution was finished");
+        // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 
 
-    }
+                WebDriver driver = DriverManager.getInstance().getDriver();
+
+                driver.get("https://andreisecuqa.host/");
+
+                String currentWindowName = driver.getWindowHandle();
+
+                driver.switchTo().newWindow(WindowType.TAB);
+
+                driver.get("https://andreisecuqa.host/");
+
+                WebElement accountIcon = driver.findElement(By.xpath("//i[@class='fa-solid fa-user']"));
+                accountIcon.click();
+
+                WebElement registerBtn = driver.findElement(By.xpath("//a[@class='dropdown-item'][normalize-space()='Register']"));
+                registerBtn.click();
+
+                String firstName = DataFakerManager.getRandomName();
+                System.out.println("The generated first name is " + firstName);
+
+                String lastName = DataFakerManager.getRandomName();
+                System.out.println("The generated last name is " + lastName);
+
+                String email = DataFakerManager.getRandomEmail();
+                System.out.println("The generated email is " + email);
+
+                String password = DataFakerManager.getRandomPassword(15, 21);
+                System.out.println("The generated password is " + password);
+
+                WebElement firstNameInput = driver.findElement(By.id("input-firstname"));
+                firstNameInput.sendKeys(firstName);
+
+                WebElement lastNameInput = driver.findElement(By.id("input-lastname"));
+                lastNameInput.sendKeys(lastName);
+
+                WebElement emailInput = driver.findElement(By.id("input-email"));
+                emailInput.sendKeys(email);
+
+                WebElement passwordInput = driver.findElement(By.id("input-password"));
+                passwordInput.sendKeys(password);
 
 
-}
+                Thread.sleep(500);
+                WebElement privacyToggleBox = driver.findElement(By.xpath("//input[@name='agree']"));
+                ScrollManager.scrollToElement(driver, privacyToggleBox);
+                privacyToggleBox.click();
+
+                WebElement continueButton = driver.findElement(By.xpath("//button[normalize-space()='Continue']"));
+                continueButton.click();
+
+
+                Thread.sleep(5000);
+
+
+                driver.close();
+
+                driver.switchTo().window(currentWindowName);
+                Thread.sleep(5000);
+
+                driver.get("https://andreisecuqa.host/");
+
+
+                driver.quit();
+
+                System.out.println("The Execution was finished");
+
+
+            }
+        }
